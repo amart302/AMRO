921782b9124ab66618a721ec8e62eaa72705cf7e
Ahmed <amartazanov892@gmail.com>
2024-07-30 17:16:47 +0300

    Main commit 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,9 +10,38 @@ whyAreWeBtn.addEventListener("click", () => {
 
 const howDoWeCleaningBtn = document.getElementById("howDoWeCleaningBtn");
 const howDoWeCleaningBlock = document.querySelector(".How_do_we_do_the_cleaning");
+const chooseBtn = document.getElementById("chooseBtn");
+
 
 howDoWeCleaningBtn.addEventListener("click", () => {
   howDoWeCleaningBlock.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+});
+
+chooseBtn.addEventListener("click", () => {
+  howDoWeCleaningBlock.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+});
+
+const servicesBtn = document.getElementById("servicesBtn");
+const additionalServicesBlock = document.querySelector(".Additional_services");
+
+servicesBtn.addEventListener("click", () => {
+  additionalServicesBlock.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+});
+
+const costCalculationBtn = document.getElementById("costCalculationBtn");
+const costCalculationBlock = document.querySelector(".Cost_calculation");
+
+costCalculationBtn.addEventListener("click", () => {
+  costCalculationBlock.scrollIntoView({
     behavior: "smooth",
     block: "start",
   });
